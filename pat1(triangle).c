8110ecf99627f60a0394334186ec3f61a431f039
Athiruban8 <115388770+Athiruban8@users.noreply.github.com>
2023-04-30 10:52:49 +0530

    Add files via upload

---
+++ pat1(triangle).c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main(){
+    int a=0,r=0,o=0,w=0,x,y,z,sum=0,i=3;
+    while(i!=0){
+        scanf("%d",&x);
+        scanf("%d",&y);
+        scanf("%d",&z);
+        sum = x+y+z;
+        if(sum!=180){
+            printf("Wrong Entry try again\n");
+            w++;
+            continue;
+        }
+        else{
+            if(x==90||y==90||z==90){
+                r++;
+            }    
+            else if(x>90||y>90||z>90){
+                o++;
+            }
+            else{
+                a++;
+            }
+            i--;
+        }
+    }
+    printf("Acute Angled Triangle: %d\n",a);
+    printf("Right Angled Triangle: %d\n",r);
+    printf("Obtuse Angled Triangle: %d\n",o);
+    printf("Wrong Entries: %d",w);
+    return 0;
+}
